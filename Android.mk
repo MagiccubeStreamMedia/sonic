fb2b4c0b9c8d716c24a2fe37e0ff0356230a5b6f
MagiccubeStreamMedia <magiccubestreammedia@gmail.com>
2021-12-27 11:40:07 +0800

    fix compile errors

--- Android.mk
+++ Android.mk
@@ -3,10 +3,10 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 #LOCAL_LDLIBS += -llog -landroid
 
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../
-LOCAL_C_INCLUDES += $(realpath $(LOCAL_PATH)/../../../)
+LOCAL_C_INCLUDES += $(LOCAL_PATH)
+LOCAL_C_INCLUDES += $(realpath $(LOCAL_PATH))
 
-LOCAL_SRC_FILES += ../../../sonic.c
+LOCAL_SRC_FILES += sonic.c
 
 LOCAL_STATIC_LIBRARIES := cpufeatures
 
