4651abbbbadc34f84f6ce9c84624e70282a351f6
MagiccubeStreamMedia <magiccubestreammedia@gmail.com>
2021-12-27 11:25:37 +0800

    change compile method

--- android/arm64-v8a/jni/Application.mk
+++
@@ -1,5 +0,0 @@
-APP_ABI := arm64-v8a
-APP_STL := c++_static
-APP_OPTIM := release
-APP_PLATFORM := android-21
-#NDK_TOOLCHAIN_VERSION=4.9
--- android/armeabi-v7a/jni/Application.mk
+++
@@ -1,6 +0,0 @@
-APP_ABI := armeabi-v7a
-APP_STL := c++_static
-APP_OPTIM := release
-APP_PLATFORM := android-14
-NDK_TOOLCHAIN_VERSION=4.9
-APP_PIE := false
